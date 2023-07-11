12d57bf0fb21e88b7853c806dab961744e112c2a
Vítor Moura <123372633+mouravitor313@users.noreply.github.com>
2023-07-11 14:20:53 -0300

    Update and rename ex4.py to [Aula 1] - ex4.py

---
+++ exercises/[Aula 1] - ex4.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um número:\n'))
+print(f'O antecessor a este número é: {n1-1} e o sucessor é {n1+1}')
--- exercises/ex4.py
+++
@@ -1,2 +0,0 @@
-n1 = int(input('Digite um número:\n'))
-print(f'O antecessor a este número é: {n1-1} e o sucessor é {n1+1}')
